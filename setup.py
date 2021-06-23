8d97cc623dc63ed919be88f305e2dfaef2a13c1c
Girard Julien <julien.girard@obspm.fr>
2021-06-23 22:20:56 +0200

    Initial commit to master with basic files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="SEDition",
+	version="0.1",
+	description="A simple tool to retrieve astronomical source SEDs",
+	license="MIT",
+	packages=find_packages(),
+	authors='Julien Girard, David Law & Ilya Yakunin (alpha order)'
+	
+
+)
